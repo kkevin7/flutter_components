21c048568d779f9ff0c5c1113fec2abaac60f4bb
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-05-17 08:56:11 -0600

    input of type password and email

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -6,7 +6,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  String _nombre;
+  String _nombre = '';
+  String _email = '';
+  String _password = '';
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +20,10 @@ class _InputPageState extends State<InputPage> {
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: <Widget>[
           _crearInput(),
+          Divider(),
+          crearEmail(),
+          Divider(),
+          crearPassword(),
           Divider(),
           _crearPesona(),
         ],
@@ -49,6 +55,45 @@ class _InputPageState extends State<InputPage> {
   Widget _crearPesona() {
     return ListTile(
       title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email es: $_email'),
+    );
+  }
+
+  Widget crearEmail() {
+    return TextField(
+      // autofocus: true,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        counter: Text('Letras ${_email.length}'),
+        hintText: 'Nombre del Email',
+        labelText: 'Correo Electrónico',
+        helperText: 'Sólo es el Email',
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.email),
+      ),
+      onChanged: (valor) => setState(() {
+        _email = valor;
+      }),
+    );
+  }
+
+  Widget crearPassword() {
+     return TextField(
+      // autofocus: true,
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        counter: Text('Letras ${_password.length}'),
+        hintText: 'Password',
+        labelText: 'Password',
+        helperText: 'Password',
+        suffixIcon: Icon(Icons.lock),
+        icon: Icon(Icons.lock),
+      ),
+      onChanged: (valor) => setState(() {
+        _password = valor;
+      }),
     );
   }
 }
