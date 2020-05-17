e444c780294327f88af994a514f4d53c3ddf23de
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-05-16 19:58:08 -0600

    starting to config the routes applications

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
-import 'package:componentes/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
- 
+
+import 'package:componentes/src/routes/routes.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Componentes App',
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      // home: HomePage(),
+      initialRoute: '/',
+      routes: getAplicationRoutes(),
+      onGenerateRoute: (setting){
+        print('Route llamda: ${setting.name}');
+        return MaterialPageRoute(
+          builder: (BuildContext context) => AlertPage()
+        );
+      },
     );
   }
 }
