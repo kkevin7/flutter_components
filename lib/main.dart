39a125502cf1777b920514e31552fe8fa6456b78
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-05-17 10:33:18 -0600

    local language configurations and import libs

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:componentes/src/routes/routes.dart';
 import 'package:componentes/src/pages/alert_page.dart';
-
 
 void main() => runApp(MyApp());
 
@@ -12,14 +12,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en'), // English
+        const Locale('es'), // Spanish
+      ],
       // home: HomePage(),
       initialRoute: '/',
       routes: getAplicationRoutes(),
-      onGenerateRoute: (setting){
+      onGenerateRoute: (setting) {
         print('Route llamda: ${setting.name}');
         return MaterialPageRoute(
-          builder: (BuildContext context) => AlertPage()
-        );
+            builder: (BuildContext context) => AlertPage());
       },
     );
   }
