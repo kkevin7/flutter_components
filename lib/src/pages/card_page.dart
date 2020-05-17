9cb5961e5227d33ae8507381a09e72d7349e3a01
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-05-16 20:23:14 -0600

    Using cards to show content

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[_cardTipo1()],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+            title: Text('Soy el titulo de la tarjeta'),
+            subtitle: Text(
+                'Esta es una subtitulo para mostrar dentro de la tarjeta que se puede mostrar con el contenido del Widget'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {},
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
